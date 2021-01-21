0fc1330f2faa3b632904bf1fe761b3d0e75704c7
Erik Spitzer <erik.janez.spitzer@gmail.com>
2021-01-21 13:05:39 +0100

    Connect clock script to html UI. Add end of session ring tone.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const pageTitle = document.querySelector("title");
 const poBtn = document.querySelector("#po-btn");
 const sbBtn = document.querySelector("#sb-btn");
 const lbBtn = document.querySelector("#lb-btn");
@@ -6,6 +7,9 @@ const setTime = document.querySelector(".set-time");
 const inputField = document.querySelector(".set-time div input");
 const controlBtns = document.querySelectorAll(".control-btn");
 
+const ringTone = new Audio("sounds/digital-alarm-clock.mp3");
+let countDownInterval;
+
 setTime.children[0].style.display = "block";
 setTime.children[1].style.display = "none";
 setTime.children[2].style.display = "none";
@@ -13,6 +17,8 @@ setTime.children[2].style.display = "none";
 poBtn.addEventListener("click", toggleSession);
 sbBtn.addEventListener("click", toggleSession);
 lbBtn.addEventListener("click", toggleSession);
+
+controlBtns[0].addEventListener("click", resetStart);
 
 function toggleSession(e) {
     if(e.target === poBtn){
@@ -42,35 +48,40 @@ function toggleSession(e) {
     }
 }
 
+function initCountDown() {
+    const sessionTime = parseInt(inputField.value);
+    const startTime = new Date();
+    const endTime = new Date().setMinutes(startTime.getMinutes() + sessionTime);
 
+    countDownInterval = setInterval(countDown, 1000);
 
-
-
-const sessionTime = parseInt(inputField.value);
-const startTime = new Date();
-const endTime = new Date().setMinutes(startTime.getMinutes() + sessionTime);
-let startTimeDiff = endTime - startTime.getTime();
-
-const countDownInterval = setInterval(countDown, 1000);
-
-function countDown() {
-    const controlTime = new Date().getTime();
-
-    let timeDiff = endTime - controlTime;
-    let m = Math.floor(timeDiff / 1000 / 60);
-    let s = Math.floor((timeDiff / 1000) % 60);
-
-    if(m === 0 && s === 0) {
-        console.log("Countdown finished!");
-        clearInterval(countDownInterval);
-    }
+    function countDown() {
+        const controlTime = new Date().getTime();
         
-    m = m < 10? "0" + m : m;
-    s = s < 10? "0" + s : s;
+        let timeDiff = endTime - controlTime;
+        let m = Math.floor(timeDiff / 1000 / 60);
+        let s = Math.floor((timeDiff / 1000) % 60);
+    
+        if(m === 0 && s === 0) {
+            console.log("Countdown finished!");
+            clearInterval(countDownInterval);
+            ringTone.play();
+        }
+            
+        m = m < 10? "0" + m : m;
+        s = s < 10? "0" + s : s;
+    
+        timeDisplay[0].innerHTML = m;
+        timeDisplay[2].innerHTML = s;
+        pageTitle.innerHTML = `${m}:${s} Pomodoro Clock`
+    }
+}
 
+function killCountDown() {
+    clearInterval(countDownInterval);
+}
 
-    timeDisplay[0].innerHTML = m;
-    timeDisplay[2].innerHTML = s;
-    console.log(`${m}:${s}`);
-
+function resetStart() {
+    killCountDown();
+    initCountDown();
 }
